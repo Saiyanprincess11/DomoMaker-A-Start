ea53ef4ecb1ae4b0cfb2badf7672dd477f64f9d9
Saiyanprincess11 <pth3035@rit.edu>
2023-03-29 23:10:22 -0400

    Finised DomoMaker B

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const helmet = require('helmet');
+const session = require('express-session');
 
 const router = require('./router.js');
 
@@ -15,7 +16,7 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/DomoMaker';
 mongoose.connect(dbURI).catch((err) => {
   if (err) {
-    console.log('Could not connect to database');
+    //console.log('Could not connect to database');
     throw err;
   }
 });
@@ -28,13 +29,23 @@ app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Session config
+app.use(session({
+  key: 'sessionid',
+  secret: 'Domo Arigato',
+  resave: false,
+  saveUninitialized: false,
+}));
+
 app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 
 router(app);
 
+// Starts server
 app.listen(port, (err) => {
   if (err) { throw err; }
-  console.log(`Listening on port ${port}`);
+  //console.log(`Listening on port ${port}`);
 });
--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -3,8 +3,13 @@ const models = require('../models');
 const { Account } = models;
 
 const loginPage = (req, res) => res.render('login');
+
 const signupPage = (req, res) => res.render('signup');
-const logout = (req, res) => res.redirect('/');
+
+const logout = (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
+};
 
 const login = (req, res) => {
   const username = `${req.body.username}`;
@@ -18,6 +23,8 @@ const login = (req, res) => {
     if (err || !account) {
       return res.status(401).json({ error: 'Wrong username or password!' });
     }
+
+    req.session.account = Account.toAPI(account);
 
     return res.json({ redirect: '/maker' });
   });
@@ -43,9 +50,10 @@ const signup = async (req, res) => {
     const hash = await Account.generateHash(pass);
     const newAccount = new Account({ username, password: hash });
     await newAccount.save();
+    req.session.account = Account.toAPI(newAccount);
     return res.json({ redirect: '/maker' });
   } catch (err) { // Otherwise handles any error thrown
-    console.log(err);
+    // console.log(err);
 
     // If username has already been used...
     if (err.code === 11000) {
