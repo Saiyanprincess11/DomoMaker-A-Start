31aa49af8b57c407f1b7864b6e71d6066ba375cc
Saiyanprincess11 <pth3035@rit.edu>
2023-04-01 13:12:28 -0400

    Finised DomoMaker C

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const compression = require('compression');
@@ -7,6 +8,8 @@ const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const helmet = require('helmet');
 const session = require('express-session');
+const RedisStore = require('connect-redis').default;
+const redis = require('redis');
 
 const router = require('./router.js');
 
@@ -16,36 +19,49 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/DomoMaker';
 mongoose.connect(dbURI).catch((err) => {
   if (err) {
-    //console.log('Could not connect to database');
+    // console.log('Could not connect to database');
     throw err;
   }
 });
 
-const app = express();
+// CReates Redis Client
+const redisClient = redis.createClient({
+  url: process.env.REDISCLOUD_URL,
+});
 
-app.use(helmet());
-app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
-app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
-app.use(compression());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+redisClient.on('error', (err) => console.log('Redis Client Error', err));
 
-// Session config
-app.use(session({
-  key: 'sessionid',
-  secret: 'Domo Arigato',
-  resave: false,
-  saveUninitialized: false,
-}));
+// Connects to redis instance
+redisClient.connect().then(() => {
+  const app = express();
 
-app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
-app.set('view engine', 'handlebars');
-app.set('views', `${__dirname}/../views`);
+  app.use(helmet());
+  app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
+  app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+  app.use(compression());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
 
-router(app);
+  // Session config
+  app.use(session({
+    key: 'sessionid',
+    store: new RedisStore({
+      client: redisClient,
+    }),
+    secret: 'Domo Arigato',
+    resave: false,
+    saveUninitialized: false,
+  }));
 
-// Starts server
-app.listen(port, (err) => {
-  if (err) { throw err; }
-  //console.log(`Listening on port ${port}`);
+  app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
+  app.set('view engine', 'handlebars');
+  app.set('views', `${__dirname}/../views`);
+
+  router(app);
+
+  // Starts server
+  app.listen(port, (err) => {
+    if (err) { throw err; }
+    // console.log(`Listening on port ${port}`);
+  });
 });
--- server/router.js
+++ server/router.js
@@ -1,18 +1,22 @@
 const controllers = require('./controllers');
+const mid = require('./middleware'); 
 
+//URL routing for app 
 const router = (app) => {
-  app.get('/login', controllers.Account.loginPage);
-  app.post('/login', controllers.Account.login);
+  //Middleware fires in order and if it passes, moves to the next function
+  //If criteria isn't met, loads different page and breaks flow 
+  app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
+  app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
 
-  app.get('/signup', controllers.Account.signupPage);
-  app.post('/signup', controllers.Account.signup);
+  app.get('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signupPage);
+  app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
 
-  app.get('/logout', controllers.Account.logout);
+  app.get('/logout', mid.requiresLogin, controllers.Account.logout);
 
-  app.get('/maker', controllers.Domo.makerPage);
-  app.post('/maker', controllers.Domo.makeDomo);
+  app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
+  app.post('/maker', mid.requiresLogin, controllers.Domo.makeDomo);
 
-  app.get('/', controllers.Account.loginPage);
+  app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
 };
 
 module.exports = router;
